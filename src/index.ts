5a3d10d1a6a2f05751139ad58fa49f8ac9747e3d
Craig Simko <craigs1701@gmail.com>
2018-04-21 14:38:08 -0400

    Updated index to call the data extractor class.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import DeckAndBoosterUrlParser from './Parsers/DeckAndBoosterUrlParser';
+import CardDetailExtractor from './Parsers/CardDetailExtractor';
+import { CardSetExtractionData } from './Types';
 
 const deckAndBoosterUrlParser = new DeckAndBoosterUrlParser();
+const cardDetailExtractor = new CardDetailExtractor();
 
 deckAndBoosterUrlParser
   .getUrls()
-  .then((res) => {
-    console.log(res);
+  .then((cardSetData: CardSetExtractionData[]) => {
+    cardDetailExtractor.getCardSets(cardSetData);
   });
