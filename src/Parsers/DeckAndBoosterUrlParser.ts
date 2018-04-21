de035905e667e1215f0dcdf39e1080c692700e50
Craig Simko <craigs1701@gmail.com>
2018-04-21 13:36:30 -0400

    Renamed CardSetList to CardSet to be more representative of what it actually is.

--- src/Parsers/DeckAndBoosterUrlParser.ts
+++ src/Parsers/DeckAndBoosterUrlParser.ts
@@ -1,15 +1,15 @@
 import * as request from 'request';
 import { JSDOM } from 'jsdom';
 import * as Q from 'q';
-import { CardSetList, CardSetType } from '../Types';
+import { CardSet, CardSetType } from '../Types';
 
 const WIKI_URL = 'http://fireemblem.wikia.com';
 const DECK_AND_BOOSTER_INDEX_URL = 'http://fireemblem.wikia.com/wiki/Fire_Emblem_0_(Cipher)';
 
 export default class DeckAndBoosterUrlParser {
-  public getUrls(): Q.Promise<CardSetList[]> {
-    const cardSetLists: CardSetList[] = [];
-    const deferred = Q.defer<CardSetList[]>();
+  public getUrls(): Q.Promise<CardSet[]> {
+    const cardSetLists: CardSet[] = [];
+    const deferred = Q.defer<CardSet[]>();
     request(DECK_AND_BOOSTER_INDEX_URL, (error, response, body) => {
       if (error) {
         deferred.reject(error);
--- src/Types.ts
+++ src/Types.ts
@@ -3,8 +3,25 @@ export enum CardSetType {
   STARTER = 'starter'
 }
 
-export interface CardSetList {
+export interface CardSet {
   name: string;
   url: string;
   type: CardSetType;
+}
+
+export interface Card {
+  name: string;
+  illustrator: string;
+  deployCost: number;
+  cardNumber: string;
+  promoteCost: number;
+  class: string;
+  attack: number;
+  affinities: string[];
+  range: number;
+  notes: string;
+  support: number;
+  quote: string;
+  skill: string[];
+  supportSkill: string[];
 }
